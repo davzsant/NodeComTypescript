4914d8dc8f2cb9cf66d5444df932b1969ede21e8
Millexs <david.lucas.santiago02@gmail.com>
2024-07-29 19:25:56 -0300

    feat: arrumado

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -2,6 +2,8 @@ import supertest from "supertest";
 import { server } from "../src/server/Server";
 import { Knex } from "../src/server/database/knex";
 
+
+
 beforeAll(async()=>{
   await Knex.migrate.latest();
 });
